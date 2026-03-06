f6b66ddf385bf234717fb2dfb8ef532340fd436c
Anders Bekkevard <anders.bekkevard@gmail.com>
2026-03-06 20:42:09 +0100

    Fix Linux full tool coverage

--- setup/linux-headless.sh
+++ setup/linux-headless.sh
@@ -6,8 +6,4 @@ run_linux_headless_layer() {
   fi
 
   log_info "Layer: linux-headless"
-  ensure_gh_apt_repo
-  apt_update_once
-  apt_install_manifest "$DOTFILES_DIR/setup/packages/apt.full.txt"
-  install_linux_release_binaries
 }
