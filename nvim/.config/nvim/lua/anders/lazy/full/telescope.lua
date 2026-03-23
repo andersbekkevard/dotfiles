04833ee5ff96e70334c20f4a83740cdfbd9bd5a1
Anders Bekkevard <anders.bekkevard@gmail.com>
2026-03-23 23:47:48 +0000

    perf improvements

--- nvim/.config/nvim/lua/anders/lazy/full/telescope.lua
+++ nvim/.config/nvim/lua/anders/lazy/full/telescope.lua
@@ -19,6 +19,8 @@ return {
 					mappings = {
 						i = {
 							["<C-c>"] = require('telescope.actions').close,
+							["<C-j>"] = require('telescope.actions').move_selection_next,
+							["<C-k>"] = require('telescope.actions').move_selection_previous,
 						},
 					},
 				},
